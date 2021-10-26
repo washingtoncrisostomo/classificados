a92eb6bb0fe8d287f19798bcacaf8d1afa6f62a3
washi <washington.parra@gmail.com>
2021-10-26 11:44:26 -0300

    Initial structure

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,7 +12,36 @@
     <link rel="text/javascript" href="assets/js/bootstrap.min.js">
     <link rel="text/javascript" href="assets/js/script.js">
 </head>
+
 <body>
-    
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="./" class="navbar-brand">Classificados</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">Cadastre-se</a></li>
+                <li><a href="">Login</a></li>
+                <li><a href="">Meus anúncios</a></li>
+                <li><a href="">Sair</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h2>Nós temos hoje 999 anúncios.</h2>
+            <p>E mais de 999 usuários cadastrados.</p>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-3">
+                <h4>Pesquisa Avançada</h4>
+            </div>
+            <div class="col-sm-9">
+                <h4>Últimos Anúncios</h4>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
