3e52d057f665716fa13fbda36e8f68060282759e
washi <washington.parra@gmail.com>
2021-10-26 11:49:59 -0300

    Pages directory

--- index.php
+++ index.php
@@ -1,47 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require 'pages/header.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Classificados</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="text/javascript" href="assets/js/jquery.min.js">
-    <link rel="text/javascript" href="assets/js/bootstrap.min.js">
-    <link rel="text/javascript" href="assets/js/script.js">
-</head>
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h2>Nós temos hoje 999 anúncios.</h2>
+        <p>E mais de 999 usuários cadastrados.</p>
+    </div>
 
-<body>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a href="./" class="navbar-brand">Classificados</a>
-            </div>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="">Cadastre-se</a></li>
-                <li><a href="">Login</a></li>
-                <li><a href="">Meus anúncios</a></li>
-                <li><a href="">Sair</a></li>
-            </ul>
+    <div class="row">
+        <div class="col-sm-3">
+            <h4>Pesquisa Avançada</h4>
         </div>
-    </nav>
-    <div class="container-fluid">
-        <div class="jumbotron">
-            <h2>Nós temos hoje 999 anúncios.</h2>
-            <p>E mais de 999 usuários cadastrados.</p>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-3">
-                <h4>Pesquisa Avançada</h4>
-            </div>
-            <div class="col-sm-9">
-                <h4>Últimos Anúncios</h4>
-            </div>
+        <div class="col-sm-9">
+            <h4>Últimos Anúncios</h4>
         </div>
     </div>
-</body>
+</div>
 
-</html>
+<?php require 'pages/footer.php'; ?>
---
+++ pages/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classificados</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="text/javascript" href="assets/js/jquery.min.js">
+    <link rel="text/javascript" href="assets/js/bootstrap.min.js">
+    <link rel="text/javascript" href="assets/js/script.js">
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="./" class="navbar-brand">Classificados</a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">Cadastre-se</a></li>
+                <li><a href="">Login</a></li>
+                <li><a href="">Meus anúncios</a></li>
+                <li><a href="">Sair</a></li>
+            </ul>
+        </div>
+    </nav>
