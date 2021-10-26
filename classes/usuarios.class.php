fe455e4f79970cf6de6979883b36a97bbc3a5164
washi <washington.parra@gmail.com>
2021-10-26 18:34:15 -0300

    Login

--- classes/usuarios.class.php
+++ classes/usuarios.class.php
@@ -19,5 +19,22 @@
                 return false;
             }
         }
+
+        public function login($email, $senha){
+            global $pdo;
+
+            $sql = $pdo->prepare("SELECT id FROM usuarios WHERE email = :email AND senha = :senha");
+            $sql->bindValue(":email", $email);
+            $sql->bindValue(":senha", md5($senha));
+            $sql->execute();
+
+            if($sql->rowCount() > 0) {
+                $dado = $sql->fetch();
+                $_SESSION['cLogin'] = $dado['id'];
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 ?>
