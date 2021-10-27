50d68360fa104bbce803dcc84aa94cb96a0beb7e
washi <washington.parra@gmail.com>
2021-10-27 11:46:31 -0300

    Editar

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -18,10 +18,25 @@ class Anuncios {
         return $array;
     }
 
+    public function getAnuncio($id){
+        $array = array();
+        global $pdo;
+
+        $sql = $pdo->prepare("SELECT * FROM anuncios WHERE id= :id");
+        $sql->bindValue(":id", $id);
+        $sql->execute();
+
+        if($sql->rowCount()>0){
+            $array = $sql->fetch();
+        }
+
+        return $array;
+    }
+
     public function addAnuncio($titulo, $categoria, $valor, $descricao, $estado){
         global $pdo;
 
-        $sql = $pdo->prepare("INSERT anuncios SET titulo = :titulo,
+        $sql = $pdo->prepare("INSERT INTO anuncios SET titulo = :titulo,
         id_categoria=:id_categoria, id_usuario=:id_usuario, descricao=:descricao, 
         valor=:valor, estado=:estado");
         $sql->bindValue(":titulo", $titulo);
@@ -30,6 +45,22 @@ class Anuncios {
         $sql->bindValue(":descricao", $descricao);
         $sql->bindValue(":valor", $valor);
         $sql->bindValue(":estado", $estado);
+        $sql->execute();
+    }
+
+    public function editAnuncio($titulo, $categoria, $valor, $descricao, $estado, $id){
+        global $pdo;
+
+        $sql = $pdo->prepare("UPDATE anuncios SET titulo = :titulo,
+        id_categoria=:id_categoria, id_usuario=:id_usuario, descricao=:descricao, 
+        valor=:valor, estado=:estado WHERE id =:id");
+        $sql->bindValue(":titulo", $titulo);
+        $sql->bindValue(":id_categoria", $categoria);
+        $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
+        $sql->bindValue(":descricao", $descricao);
+        $sql->bindValue(":valor", $valor);
+        $sql->bindValue(":estado", $estado);
+        $sql->bindValue(":id", $id);
         $sql->execute();
     }
 
