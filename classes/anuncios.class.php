d3c6a6e102d56a088c441421ff269eb68959ed52
washi <washington.parra@gmail.com>
2021-10-26 23:45:07 -0300

    Add anuncios

--- classes/anuncios.class.php
+++ classes/anuncios.class.php
@@ -5,8 +5,8 @@ class Anuncios {
 
         $array = array();
         $sql = $pdo->prepare("SELECT *,
-         (select anuncios_images.url from anuncios_images.url where anuncios_images
-         .id_anuncio = anuncio.id limit 1) as url 
+         (select anuncios_imagens.url from anuncios_imagens where anuncios_imagens
+         .id_anuncio = anuncios.id limit 1) as url 
          FROM anuncios WHERE id_usuario= :id_usuario");
         $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
         $sql->execute();
@@ -16,6 +16,21 @@ class Anuncios {
         }
 
         return $array;
+    }
+
+    public function addAnuncio($titulo, $categoria, $valor, $descricao, $estado){
+        global $pdo;
+
+        $sql = $pdo->prepare("INSERT anuncios SET titulo = :titulo,
+        id_categoria=:id_categoria, id_usuario=:id_usuario, descricao=:descricao, 
+        valor=:valor, estado=:estado");
+        $sql->bindValue(":titulo", $titulo);
+        $sql->bindValue(":id_categoria", $categoria);
+        $sql->bindValue(":id_usuario", $_SESSION['cLogin']);
+        $sql->bindValue(":descricao", $descricao);
+        $sql->bindValue(":valor", $valor);
+        $sql->bindValue(":estado", $estado);
+        $sql->execute();
     }
 }
 ?>
--- meus-anuncios.php
+++ meus-anuncios.php
@@ -2,9 +2,7 @@
 <?php
 if (empty($_SESSION['cLogin'])) {
 ?>
-    <script type="text/javascript">
-        window.location.href = "login.php";
-    </script>
+    <script type="text/javascript">window.location.href = "login.php";</script>
 <?php
     exit;
 }
